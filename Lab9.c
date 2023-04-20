57c92a019c214909c856c500c948be2ca2278258
AnastasiaSmi203 <116147826+AnastasiaSmi203@users.noreply.github.com>
2023-04-20 13:34:03 +0500

    Update Lab9.c

--- Lab9.c
+++ Lab9.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-    
-    char str[100];
+    char str[1000];
     printf("Input: ");
     gets(str);
-    printf("Output: ");
-    int  flag = 0, count = 0, number_letters = 0, sum=0;
-    
-    if(str[0] == '\n') return 0;
-    do{
-        while (str[count]!='\0'){
-            char result_str[100];
-          while(isdigit(str[count])!=0){
-                result_str[number_letters]= str[count];
-                number_letters= number_letters + 1;
-                count=count+1;
-          }
-          count=count+1;
-          
-          if ((str[count-number_letters-2] == ' ') && (number_letters!=0) && (str[count-1]==' ')){
-            sum = sum + atoi(result_str);
-            memset(result_str,0,100);  
-          }
-          if ((str[0]!= ' ') && (isdigit(str[1])!=0) && (str[count-1]==' ')){
-            sum = sum + atoi(result_str);
-            memset(result_str,0,100);  
-          }
-          int len = strlen(str);
-          if ((str[len]!= ' ') && (isdigit(str[len-1])!=0) && (str[count-number_letters-2]==' ')){
-            sum = sum + atoi(result_str);
-            memset(result_str,0,100);  
-          }
-          number_letters = 0;
+
+    int sum = 0; // Переменная для хранения суммы чисел
+    char* word = strtok(str, " "); // Разделение строки на слова по пробелам с использованием функции strtok()
+
+    if (str[0] == '\n') return 0; // Проверка на пустую строку (если первый символ - символ новой строки, то возвращаем 0 и завершаем программу)
+
+    while (word != NULL) { // Цикл обработки каждого слова
+        char* endptr;
+        long num = strtol(word, &endptr, 10); // Преобразование текущего слова в число с использованием функции strtol()
+
+        if (endptr == word || *endptr != '\0' || (num == 0 && !isdigit(word[0]))) {
+            // Проверка на успешное преобразование и наличие пробелов перед и после числа, а также исключение случаев, когда слово содержит другие символы помимо чисел
+            // (в данном случае ничего не делаем, пропускаем это слово)
+        } else if (num > 0) {
+            sum += num; // Добавление числа к сумме, если оно положительное
         }
-        if (sum!=0){
-            printf("%d\n", sum);
-        }
-        else{
-            printf("String is empty or don't have numbers\n");
-        }
-        flag = 1;
-    }while(!flag);
+        word = strtok(NULL, " "); // Получение следующего слова
+    }
+
+    if (sum > 0) {
+        printf("Output: %d\n", sum); // Вывод суммы положительных чисел
+    } else {
+        printf("String is empty or doesn't have numbers\n"); // Вывод сообщения, если строка пустая или не содержит чисел
+    }
+
+    return 0;
 }
