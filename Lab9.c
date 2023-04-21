42c9f06310598d524a3f3ed669aa1db6705a094e
AnastasiaSmi203 <116147826+AnastasiaSmi203@users.noreply.github.com>
2023-04-22 01:41:55 +0500

    Update Lab9.c

--- Lab9.c
+++ Lab9.c
@@ -2,35 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-int output(int sum){
-    if (sum > 0) {
-        printf("Output: %d\n", sum); 
-    } else {
-        printf("String is empty or doesn't have numbers\n"); 
+int Empty(char* str) {
+  if (str[0] == '\n')
+    return 1;
+
+  return 0;
+}
+
+int Sum(char* str, int* sum) {
+  char* word = strtok(str, " ");
+  while (word != NULL) {
+    char* endptr;
+    long num =
+        strtol(word, &endptr, 10);
+    if (endptr == word || *endptr != '\0' || (num == 0 && !isdigit(word[0]))) {
+    } else if (num > 0) {
+      *sum += num; 
     }
-    return 0;
+    word = strtok(NULL, " ");
+  }
+
+  if (*sum <= 0) {
+    return -1;
+  }
+
+  return 0;
 }
 
 int main() {
-    char str[1000];
-    printf("Input: ");
-    gets(str);
+  char str[1000];
+  printf("Input: ");
+  gets(str);
 
-    int sum = 0;
-    char* word = strtok(str, " "); 
+  int sum = 0;
 
-    if (str[0] == '\n') return 0; 
-    while (word != NULL) { 
-        char* endptr;
-        long num = strtol(word, &endptr, 10); 
-
-        if (endptr == word || *endptr != '\0' || (num == 0 && !isdigit(word[0]))) {
-        } else if (num > 0) {
-            sum += num; 
-        }
-        word = strtok(NULL, " "); 
-    }
-    printf(output(sum));
-
+  if (Empty(str))
     return 0;
+
+  if (Sum(str, &sum) == -1)
+    printf(
+        "String is empty or doesn't have numbers\n");
+  else
+    printf("Output: %d\n", sum);
+
+  return 0;
 }
